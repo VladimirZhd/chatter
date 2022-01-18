3fc50dede221673077defb2d680c44efdece1c54
Zhdanov <vzhdanov@byui.edu>
2022-01-18 14:58:49 -0700

    added registration and login functions

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,37 @@
 import { Link } from "react-router-dom";
+import { auth, db } from "../services/firebase";
+import { signOut } from 'firebase/auth';
+import { updateDoc, doc } from 'firebase/firestore';
+import { useContext } from 'react';
+import { AuthContext } from "../context/auth";
+import { useNavigate } from "react-router-dom";
 
-const Navbar = (props) => {
+const Navbar = () => {
+    const navigate = useNavigate();
+    const { user } = useContext(AuthContext);
+    const handleSignOut = async () => {
+        await updateDoc(doc(db, 'users', auth.currentUser.uid), {
+            isOnline: false
+        });
+        await signOut(auth);
+        navigate('/login');
+    };
+
     return (
         <nav>
-            <h3><Link to='/'>Messenger</Link></h3>
+            <h3><Link to='/'>Chatter</Link></h3>
             <div>
-                <Link to='register'>Register</Link>
-                <Link to='/login'>Login</Link>
+                {user ? (
+                    <>
+                        <Link to='profile'>Profile</Link>
+                        <button className="btn" onClick={handleSignOut}>Logout</button>
+                    </>) : (
+                    <>
+                        <Link to='register'>Register</Link>
+                        <Link to='/login'>Login</Link>
+                    </>
+                )
+                }
             </div>
         </nav>
     );
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,53 +1,46 @@
 import { useState } from "react";
-import { createUserWithEmailAndPassword } from 'firebase/auth';
-import { setDoc, doc, Timestamp } from 'firebase/firestore';
+import { useNavigate } from "react-router-dom";
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { updateDoc, doc } from 'firebase/firestore';
 import { auth, db } from "../services/firebase";
-import { useNavigate } from 'react-router-dom';
 
-const Register = (props) => {
+const Login = () => {
     const [data, setData] = useState({
-        name: '',
         email: '',
         password: '',
         error: '',
         loading: false
     });
-    const { name, email, password, error, loading } = data;
+
     const navigate = useNavigate();
+
+    const { email, password, error, loading } = data;
+
     const handleChange = (event) => {
         setData({ ...data, [event.target.name]: event.target.value });
     };
 
     const handleSubmit = async (event) => {
         event.preventDefault();
-        setData({ ...data, error: '', loading: true });
-        if (!name || !password || !email) {
+        if (!email || !password) {
             setData({ ...data, error: 'All fields are required' });
         }
         try {
-            const result = await createUserWithEmailAndPassword(auth, email, password);
-            await setDoc(doc(db, 'users', result.user.uid), {
-                uid: result.user.uid,
-                name,
-                email,
-                createdAt: Timestamp.fromDate(new Date()),
+            const result = await signInWithEmailAndPassword(auth, email, password);
+            await updateDoc(doc(db, 'users', result.user.uid), {
                 isOnline: true
             });
-            setData({ name: '', email: '', password: '', error: '', loading: false });
+            setData({ email: '', password: '', error: '', loading: false });
             navigate('/');
         } catch (error) {
-            setData({ ...data, error: error.message, loading: false });
+            setData({ ...data, error: error.message });
         }
     };
 
     return (
         <section>
-            <h3>Create an account</h3>
+            <h3>Login to your account</h3>
             <form className="form" onSubmit={handleSubmit}>
-                <div className="input_container">
-                    <label htmlFor="name">Name</label>
-                    <input type='text' name='name' value={name} onChange={handleChange} />
-                </div>
                 <div className="input_container">
                     <label htmlFor="email">Email</label>
                     <input type='email' name='email' value={email} onChange={handleChange} />
@@ -58,11 +51,11 @@ const Register = (props) => {
                 </div>
                 {error && <p className="error">{error}</p>}
                 <div className="btn_container">
-                    <button type='submit' className="btn">{loading ? 'Creating...' : 'Register'}</button>
+                    <button type='submit' className="btn">{loading ? 'Logging in ...' : 'Login'}</button>
                 </div>
             </form>
         </section>
     );
 };
 
-export default Register;
+export default Login;
